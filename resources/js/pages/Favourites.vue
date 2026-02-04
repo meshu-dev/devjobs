69af1b25f43cb4312a02c61ecd228b900ff40fd8
Mesh <contact@devpush.io>
2026-02-04 21:55:54 +0000

    Add Card component

---
+++ resources/js/pages/Favourites.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import JobTable from '@/components/JobTable.vue'
+
+defineProps({ jobs: Object })
+</script>
+
+<template>
+  <h1>Favourites</h1>
+  <JobTable :jobs="jobs" />
+</template>
